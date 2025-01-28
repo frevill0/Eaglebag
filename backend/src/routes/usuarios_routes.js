b8a214bd61eca19958a935bc76c1c479368cab15
unknown <administrator@EST-30-08.quitotenis.local>
2025-01-28 12:53:02 -0500

    proteccion de rutas de usuarios

--- backend/src/routes/usuarios_routes.js
+++ backend/src/routes/usuarios_routes.js
@@ -6,14 +6,14 @@ import {
   actualizarUsuario, 
   obtenerUsuarios 
 } from '../controllers/usuarios.js';
-import { verifyToken } from '../middlewares/autenticacion.js';
+import { verifyToken, isAdmin } from '../middlewares/autenticacion.js';
 
 const router = express.Router();
 
 router.post('/login', iniciarSesion);
-router.post('/crear', crearUsuario);
-router.delete('/eliminar/:id', verifyToken, eliminarUsuario);
-router.put('/actualizar/:id', verifyToken, actualizarUsuario);
+router.post('/crear', verifyToken, isAdmin, crearUsuario);
+router.delete('/eliminar/:id', verifyToken, isAdmin, eliminarUsuario);
+router.put('/actualizar/:id', verifyToken, isAdmin, actualizarUsuario);
 router.get('/obtener', verifyToken, obtenerUsuarios);
 
 export default router; 
